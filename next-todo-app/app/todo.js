d34b45e826a21e13b4e915193511b7f275755c4d
sandeshjadhav5 <jadhavs932@gmail.com>
2023-05-06 10:39:37 +0530

    added update function

--- next-todo-app/app/todo.js
+++ next-todo-app/app/todo.js
@@ -1,13 +1,32 @@
 "use client";
 import React from "react";
 
+async function update(id, isDone) {
+  console.log("isDone", isDone);
+  try {
+    await fetch(`http://localhost:3001/api/todo/update`, {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify({ id, isDone }),
+    });
+  } catch (err) {
+    console.log("Err=>", err);
+  }
+}
+
 function Todo({ todo }) {
   return (
-    <div>
-      <input type="checkbox" onChange={() => {}} />
-      {todo.name}
-      <button>Delete</button>
-    </div>
+    <>
+      <div style={{ display: "flex", margin: "0.5rem" }}>
+        <input
+          type="checkbox"
+          onChange={(e) => update(todo.id, e.target.checked)}
+          style={{ margin: "0.5rem" }}
+        />
+        <span style={{ margin: "0.5rem" }}>{todo.name}</span>
+        <button style={{ margin: "0.5rem" }}>Delete</button>
+      </div>
+    </>
   );
 }
 
