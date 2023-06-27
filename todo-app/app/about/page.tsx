4f440c9c048d4caac7b90e32fa32ea43dd368af4
sandeshjadhav5 <jadhavs932@gmail.com>
2023-06-27 23:26:42 +0530

    made some basic changes

---
+++ todo-app/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>This is about Page</h1>
+    </div>
+  );
+}
